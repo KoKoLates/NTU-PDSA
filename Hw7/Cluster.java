9c1184cfc048a5b0919f5befa31f447fa4d28be0
KoKoLates <the21515@gmail.com>
2022-11-29 14:34:16 +0800

    score 100

--- Hw7/Cluster.java
+++ Hw7/Cluster.java
@@ -2,48 +2,66 @@ import java.util.*;
 import java.util.List;
 
 class Cluster {
+    /**
+     * The object Point to store the point data info and method like
+     * distance calculate, compare and find the centroid of two points.
+     */
     private static class Point implements Comparable<Point> {
         private final int weight;
         private final double x, y;
+        public static final Comparator<Point> X_ORDER = new XOrder();
+        public static final Comparator<Point> Y_ORDER = new YOrder();
         public Point(double x, double y, int weight) {
             this.x = x; this.y = y; this.weight = weight;
         }
 
+        /**
+         * Calculate the distance between two points.
+         * @param p1 the point1.
+         * @param p2 the point2.
+         * @return the distance between point1 and point2.
+         */
         public static double distance(Point p1, Point p2) {
-                return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-            }
-
-            public static Point findCentroid(Point p1, Point p2) {
-                double x = ((p1.x * p1.weight + p2.x * p2.weight) / (p1.weight + p2.weight));
-                double y = ((p1.y * p1.weight + p2.y * p2.weight) / (p1.weight + p2.weight));
-                return new Point(x, y, (p1.weight + p2.weight));
-            }
-
-            public int compareTo(Point that) {
-                return this.y == that.y ? Double.compare(this.x, that.x) : Double.compare(this.y, that.y);
-            }
-
-            public static final Comparator<Point> X_ORDER = new XOrder();
-
-        private static class XOrder implements Comparator<Point> {
-                public int compare(Point p1, Point p2) {
-                    return Double.compare(p1.x, p2.x);
-                }
-            }
-
-            public static final Comparator<Point> Y_ORDER = new YOrder();
-
-        private static class YOrder implements Comparator<Point> {
-                public int compare(Point p1, Point p2) {
-                    return Double.compare(p1.y, p2.y);
-                }
-            }
+            return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
         }
 
+        /**
+         * Find the centroid between of two points.
+         * @param p1 the point1.
+         * @param p2 the point2.
+         * @return the centroid point of point1 and point2.
+         */
+        public static Point findCentroid(Point p1, Point p2) {
+            double x = ((p1.x * p1.weight + p2.x * p2.weight) / (p1.weight + p2.weight));
+            double y = ((p1.y * p1.weight + p2.y * p2.weight) / (p1.weight + p2.weight));
+            return new Point(x, y, (p1.weight + p2.weight));
+        }
+
+        public int compareTo(Point that) {
+            return this.y == that.y ? Double.compare(this.x, that.x) : Double.compare(this.y, that.y);
+        }
+
+        private static class XOrder implements Comparator<Point> {
+                public int compare(Point p1, Point p2) {return Double.compare(p1.x, p2.x);}
+        }
+
+        private static class YOrder implements Comparator<Point> {
+                public int compare(Point p1, Point p2) {return Double.compare(p1.y, p2.y);}
+            }
+    }
+
+    /**
+     * Using the divide and conquer method to find the closest pair.
+     */
     private static class ClosestPair {
         public Point p1, p2;
         public double min = Double.POSITIVE_INFINITY;
 
+        /**
+         * Constructor for the Closest pair object.
+         * @param points  The points to be found.
+         * @param n the size (length) of points.
+         */
         public ClosestPair(ArrayList<Point> points, int n) {
             Point[] px = new Point[n], py = new Point[n], aux = new Point[n];
             for(int i = 0; i < n; i++) px[i] = points.get(i);
@@ -91,6 +109,7 @@ class Cluster {
         }
 
     }
+
     public List<double[]> cluster(List<int[]> points, int cluster_num) {
         ArrayList<Point> p = new ArrayList<>();
         for(int[] i: points) p.add(new Point(i[0], i[1], 1));
